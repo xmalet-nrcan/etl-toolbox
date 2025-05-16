aca5ab5c947db52b3988ca0c0a7833dddd649a2f
xaviermalet <xavier.malet@nrcan-rncan.gc.ca>
2025-05-16 16:47:42 -0400

    Add GeoPackageDataReader to support GeoPackage file reading
    Introduce a new reader class, `GeoPackageDataReader`, extending `BaseDataReader`. This class adds functionality for reading GeoPackage files using GeoPandas and Fiona, supporting layer selection and encoding options. It also provides properties to access available layers and

---
+++ nrcan_etl_toolbox/etl_toolbox/reader/geopackage_reader.py
@@ -0,0 +1,36 @@
+import fiona
+import geopandas as gpd
+
+from nrcan_etl_toolbox.etl_toolbox.reader.base_reader import BaseDataReader
+
+
+class GeoPackageDataReader(BaseDataReader):
+
+    def __init__(self, input_source, encoding='utf-8', layer=None):
+        super().__init__(input_source)
+        self._layer = layer
+        self._layers = None
+        self._encoding = encoding
+
+    def _read_data(self, layer, encoding='utf-8'):
+        self._dataframe = gpd.read_file(self._input_source
+                                        , layer=layer
+                                        , encoding=layer)
+
+    def read_layer(self, layer, encoding='utf-8') -> gpd.GeoDataFrame:
+        self._layer = layer
+        self._encoding = encoding
+        self._read_data(layer, encoding)
+        return self._dataframe
+
+    @property
+    def dataframe(self) -> gpd.GeoDataFrame:
+        if self._dataframe is None:
+            self._read_data(self._layer, self._encoding)
+        return self._dataframe
+
+    @property
+    def layers(self):
+        if self._layers is None:
+            self._layers = fiona.listlayers(self._input_source)
+        return self._layers
