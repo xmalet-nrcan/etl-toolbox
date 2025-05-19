940f1049bef7b92decc7de84ce14dea759d3a5e4
xaviermalet <xavier.malet@nrcan-rncan.gc.ca>
2025-05-19 12:04:46 -0400

    Refactor tests to use temporary directories for logging.
    Updated `CustomLogger` test cases to utilize `tempfile.TemporaryDirectory` for file path management, ensuring more isolated and cleaner test environments. Adjusted assertions to fit the new temporary directory structure and removed hardcoded paths.

--- tests/test_etl_logger.py
+++ tests/test_etl_logger.py
@@ -5,18 +5,21 @@ import os
 import pathlib
 import shutil
 
+import tempfile
+
 from nrcan_etl_toolbox.etl_logging.etl_logger import CustomLogger
 
+TMP_DIR = pathlib.Path(tempfile.TemporaryDirectory().name)
 
 def test_default_logger_initialization():
-    logger = CustomLogger(name="test_logger")
+    logger = CustomLogger(name="test_logger", file_path=TMP_DIR)
     assert logger.name == "test_logger"
     assert logger._logger_type == "default"
     assert isinstance(logger.formatter, logging.Formatter)
 
 
 def test_logger_basic_initialization():
-    logger = CustomLogger(name="basic_logger")
+    logger = CustomLogger(name="basic_logger", file_path=TMP_DIR)
     assert logger.name == "basic_logger"
     assert logger._logger_type == "default"
     assert logger.level == logging.NOTSET
@@ -24,16 +27,24 @@ def test_logger_basic_initialization():
 
 
 def test_logger_with_custom_type():
-    logger = CustomLogger(name="custom_logger", logger_type="simple")
+    logger = CustomLogger(name="custom_logger", file_path=TMP_DIR, logger_type="simple")
     assert logger.name == "custom_logger"
     assert logger._logger_type == "simple"
     assert isinstance(logger.formatter, logging.Formatter)
 
 
 def test_logger_file_path_creation():
-    temp_dir = pathlib.Path("test_logs")
-    logger = CustomLogger(name="file_logger", file_path=temp_dir, logger_file_name="test.log")
-    assert logger._file_path == os.path.join(temp_dir, "file_logger_test.log")
-    assert temp_dir.exists()
+    logger_file_name = 'test.log'
+    logger_name = "file_logger"
+    composed_logger_file_name = f"{logger_name}_{logger_file_name}"
+    tmp_file_dir = pathlib.Path(TMP_DIR) / "test_logger_file_path_creation"
+
+
+    logger = CustomLogger(name="file_logger",
+                          file_path=tmp_file_dir,
+                          logger_file_name=logger_file_name)
+    assert logger._file_path == os.path.join(tmp_file_dir, composed_logger_file_name)
+    assert tmp_file_dir.exists()
+    assert  tmp_file_dir / composed_logger_file_name in tmp_file_dir.iterdir()
     del logger
-    shutil.rmtree(temp_dir, ignore_errors=True)
+    shutil.rmtree(tmp_file_dir, ignore_errors=True)
