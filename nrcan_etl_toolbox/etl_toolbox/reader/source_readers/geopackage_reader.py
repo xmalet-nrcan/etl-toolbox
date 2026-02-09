9ce7cd27beaaa4122be6e684cd4e3e8012edbb28
Xavier Malet <xavier.malet@nrcan-rncan.gc.ca>
2026-02-09 13:03:44 -0500

    feat: enhance GeoPackage data reading with improved error handling and validation

--- nrcan_etl_toolbox/etl_toolbox/reader/source_readers/geopackage_reader.py
+++ nrcan_etl_toolbox/etl_toolbox/reader/source_readers/geopackage_reader.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import geopandas as gpd
 
 from nrcan_etl_toolbox.etl_toolbox.reader.source_readers.base_reader import BaseDataReader
@@ -9,9 +11,16 @@ class GeoPackageDataReader(BaseDataReader):
         self._layer = layer
         self._layers = None
         self._encoding = encoding
+        assert pathlib.Path(input_source).exists(), "Input GeoPackage file does not exist."
 
     def _read_data(self, layer, encoding="utf-8"):
-        self._dataframe = gpd.read_file(self._input_source, layer=layer)
+        if layer is None:
+            raise ValueError("Layer name must be provided to read data from a GeoPackage.")
+        if layer not in self.layers :
+            raise ValueError(f"Layer '{layer}' not found in the GeoPackage. Available layers: {self.layers}")
+        if len(self.layers) == 0:
+            raise ValueError(f"GeoPackage {self._input_source} has no feature layers to read.")
+        self._dataframe = gpd.read_file(self._input_source, layer=layer, encoding=encoding)
 
     def read_layer(self, layer, encoding="utf-8") -> gpd.GeoDataFrame:
         self._layer = layer
@@ -36,6 +45,7 @@ class GeoPackageDataReader(BaseDataReader):
                 cursor = conn.execute("""
                                       SELECT table_name
                                       FROM gpkg_contents
+                                          where data_type = 'features'
                                       ORDER BY table_name;
                                       """)
                 self._layers = [row[0] for row in cursor.fetchall()]
